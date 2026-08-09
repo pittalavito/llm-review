29736462e66c566f48a11de0c06ea5f0c34077d4
vito pittalà <vito.pittala.extern@adesso.it>
2026-08-09 17:05:12 +0200

    Load papers

--- src/domain/prompt/preset_default.py
+++ src/domain/prompt/preset_default.py
@@ -7,7 +7,7 @@ PresetSeed = tuple[str, str, str, list[tuple[InstructionType, str]], str]
 DEFAULT_PRESET_SEEDS: list[PresetSeed] = [
     (
         AgentRole.REVIEWER,
-        "default",
+        "iclr_v1",
         "iclr_v2",
         [
             (InstructionType.VENUE, ICLR),
@@ -16,21 +16,21 @@ DEFAULT_PRESET_SEEDS: list[PresetSeed] = [
     ),
     (
         AgentRole.META_REVIEWER,
-        "default",
+        "iclr_v1",
         "iclr_v2",
         [(InstructionType.VENUE, ICLR)],
         "Campaign meta-reviewer: base template, ICLR venue",
     ),
     (
         AgentRole.AREA_CHAIR,
-        "default",
+        "iclr_v1",
         "iclr_v2",
         [(InstructionType.VENUE, ICLR)],
         "Campaign area chair: base template, ICLR venue",
     ),
     (
         AgentRole.AUTHOR_AGENT,
-        "default",
+        "iclr_v1",
         "iclr_v2",
         [],
         "Campaign author: the base template, no persona instructions",
